2bdefe50f9b5e8b5d4bc718baaf568a682d2ddc0
RicardoVercet <ricardobieb01@gmail.com>
2025-06-28 22:25:41 +0530

    rustlings: control flow practice

--- Rust/exercise/src/main.rs
+++ Rust/exercise/src/main.rs
@@ -1,26 +1,74 @@
-use std::io;
+//use std::io;
+
+// convert temperatures Fahrenheit and Celcius
+// Generate the nth Fibonacci number
+// print the lyrics to the Christmas carol "The twelve days of christmas"
+// taking advantage of the repetition in the song
 
 fn main() {
-    println!("Some Arithmetics");
+    println!("Some more code");
     
-    let a = [1, 2, 3, 4, 5];
+    //println!("Enter a number : ");
     
-    println!("please enter an array index.");
+    //let mut inp = String::new();
+    //io::stdin().read_line(&mut inp)
+      //      .expect("failed to fetch input");
     
-    let mut index = String::new();
     
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line");
-        
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Index entered was not a number");
-        
-    let element = a[index];
-    
-    println!("value at index {index} : {element}");
-    
+    //let n: i128 = inp.trim().parse().expect("enter numbr");
+    // nth fib
+    //println!("The fib of {n} is {}", fib(n));
 
+    christmas_carol();
 }
+
+// Ex: 1
+#[allow(dead_code)]
+fn f_to_c(f: i16) -> f32 {
+    ((f - 32) as f32) * (5 as f32 / 9 as f32)
+}
+
+// Ex: 2
+#[allow(dead_code)]
+fn fib(n: i128) -> i128 {
+    let mut _prev: i128 = 0;
+    let mut last: i128 = 0;
+    let mut curr: i128 = 0;
+    for num in 1..=n {
+        if num == 1 {
+            curr += 0;
+            last = 0;
+        } else if num == 2 {
+            curr += 1;
+            last = 0;
+        } else {
+            _prev = last;
+            last = curr;
+            curr = _prev + last;
+        }
+    }
+    curr
+}
+
+fn pr(s: &str) {
+    println!("{}", s);
+}
+
+fn christmas_carol() {
+    let sec_line = "My true love sent to me";
+    
+    pr("Song: Twelve Days Of Christmas");
+    println!();
+    // seg 1
+    main_line("first");
+    pr(sec_line);
+    let _1 = "A partridge in a pear tree";
+    pr(_1);
+    
+}
+
+fn main_line(ln: &str) {
+    println!("On the {ln} day of Christmas");
+}
+
+
