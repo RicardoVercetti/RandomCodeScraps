2ffe937548c12eb36bfd0e847c6df85f121ab4b6
RicardoVercet <ricardobieb01@gmail.com>
2025-08-15 22:49:52 +0530

    rust - simple To-Do list base code

--- Rust/simple_todo_list/src/main.rs
+++ Rust/simple_todo_list/src/main.rs
@@ -11,6 +11,103 @@
 // File I/O with std::fs
 // Serialization to simple txt format line by line
 
+//use std::io;
+use std::fs::OpenOptions;
+use std::fs::read_to_string;
+use std::io::Write;
+use std::io;
+
+//#[derive(Debug)]
+struct Task {
+    description: String,
+}
+
+impl Task {
+    fn new(desc: String) -> Self {
+        Task {
+            description: desc
+        }
+    }
+    
+    fn display_data(&self) {
+        println!("Task desc : {}", self.description);
+    }
+}
+
+fn load_tasks() -> Vec<Task> {
+    let mut tasks = Vec::new();
+    if let Ok(contents) = read_to_string("tasks.txt") {
+        for line in contents.lines() {
+            tasks.push(Task::new(line.to_string()));
+        }
+    }
+    tasks
+}
+
+fn save_tasks(tasks: &Vec<Task>) {
+    let mut file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open("tasks.txt")
+            .expect("Could not open file");
+            
+            for task in tasks {
+                writeln!(file, "{}",task.description)
+                    .expect("Could not write to file");
+            }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Heyy, this runs :) ");
+    
+    loop {
+        
+        println!("\n--- To-Do List ---");
+        
+        ////println!("-- Available tasks --");
+        //for task in load_tasks() {
+        //    task.display_data();
+        //}
+        
+        println!("H: 1 for adding tasks, 2 for viewing tasks, q for quit.");
+        
+        let mut choice = String::new();
+        io::stdin().read_line(&mut choice).unwrap();
+        
+        let choice = choice.trim();
+        
+        match choice {
+            "1" => {    // add a task
+                let mut input = String::new();
+                println!("Enter the task description : ");
+                io::stdin().read_line(&mut input).unwrap();
+                
+                let mut tasks = load_tasks();
+                tasks.push(Task::new(input.trim().to_string()));
+                save_tasks(&tasks);
+                println!("Task added!");
+                
+            },
+            "2" => { // List tasks
+                println!("--------------------");
+                for (i, task) in load_tasks().iter().enumerate() {
+                    println!("{}: {}", i+1, task.description);
+                }
+                println!("--------------------");
+                //println!("-- thats all there is --");
+            },
+            "q" => {
+                println!("Goodbye...!");
+                break;
+            },
+            
+            _ => {
+                println!("Invalid value, please enter again....");
+                continue;
+            }
+        }
+        
+    }
+    
 }
