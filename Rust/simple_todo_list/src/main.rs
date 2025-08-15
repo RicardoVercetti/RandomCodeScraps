bd87e6cdd5c5e9c4a2a2683eed8e4b61ac3f7fbe
RicardoVercet <ricardobieb01@gmail.com>
2025-08-15 18:10:51 +0530

    simple todo list

---
+++ Rust/simple_todo_list/src/main.rs
@@ -0,0 +1,16 @@
+// A simple To-Do list app
+// 1. lets you add tasks
+// 2. lets you list all tasks
+// 3. saves tasks to a file
+// 4. loads tasks from the file on startup
+
+
+// Things to learn
+// structs
+// Vec
+// File I/O with std::fs
+// Serialization to simple txt format line by line
+
+fn main() {
+    println!("Hello, world!");
+}
