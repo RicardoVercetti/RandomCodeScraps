b88aa16aef1d94e86e318ddefb3f49c99096dc08
RicardoVercet <ricardobieb01@gmail.com>
2025-08-15 13:56:16 +0530

    rust - blog bin

---
+++ Rust/blog_bin/src/main.rs
@@ -0,0 +1,25 @@
+// main.rs
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop{}
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop{}
+}
+
+
+//fn main() {
+    //println!("Hello, world!");
+//}
+
+
+
