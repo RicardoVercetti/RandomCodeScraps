92d565babdd02d3f6e38d4e27a909508b13889b9
RicardoVercet <ricardobieb01@gmail.com>
2025-08-18 19:22:13 +0530

    rust - library manager task 2

--- Rust/library_manager/src/main.rs
+++ Rust/library_manager/src/main.rs
@@ -49,9 +49,18 @@ impl Book {
     }
     
     fn edit_book(&mut self, title: String, author: String, year: u32) {
-        self.title = title;
-        self.author = author;
-        self.year = year;
+        // if an empty string is passed, it means keep the old data
+        if title.len() > 0 {
+            self.title = title;
+        }
+        
+        if author.len() > 0 {
+            self.author = author;
+        }
+        
+        if year == 0 {    // zero can't be a valid year
+            self.year = year;
+        }
     }
 }
 
@@ -61,7 +70,7 @@ fn main() {
     
     'mainloop: loop {
         println!("--- Library Manager ---");
-        println!("1: Add book, 2: List Book, 3: Borrow Book, 4: Return book, q: Quit");
+        println!("1: Add book, 2: Edit book, 3: List Book, 4: Borrow Book, 5: Return book, q: Quit");
         
         let mut choice = String::new();
         io::stdin().read_line(&mut choice).unwrap();
@@ -84,7 +93,13 @@ fn main() {
                 println!("Enter the Year: ");
                 io::stdin().read_line(&mut year).unwrap();
                 let year: u32 = match year.trim().parse() {
-                    Ok(yr) => yr,
+                    Ok(yr) => {
+                        if yr == 0 {
+                            println!("Year must be a non-zero value!");
+                            continue 'mainloop;
+                        }
+                        yr
+                    },
                     Err(_) => {
                         println!("Year have to be completely numerical!");
                         continue 'mainloop;
@@ -95,6 +110,59 @@ fn main() {
                 println!("Book added successfully");
             },
             "2" => {
+              // Edit book
+              let mut id = String::new();
+              println!("Enter the ID to edit:");
+              io::stdin().read_line(&mut id).unwrap();
+              
+              let id: usize = match id.trim().parse() {
+                  Ok(val) => val,
+                  Err(_) => {
+                      println!("Enter a valid ID");
+                      continue 'mainloop;
+                  }
+              };
+              
+              if id > books.len() {
+                  println!("Such ID doesn't exist : {}", id);
+                  continue 'mainloop;
+              }
+              
+              // take title, author, year
+              let mut title = String::new();
+              let mut author = String::new();
+              let mut year = String::new();
+              
+              println!("Enter the new title: ");
+              io::stdin().read_line(&mut title).unwrap();
+              let title = title.trim();
+              
+              println!("Enter the new author name: ");
+              io::stdin().read_line(&mut author).unwrap();
+              let author = author.trim();
+              
+              println!("Enter the year: ");
+              io::stdin().read_line(&mut year).unwrap();
+              let year: u32 = match year.trim().parse() {
+                  Ok(val) => val,
+                  Err(e) => {
+                      println!("Error: {}", e);
+                      println!("Can't update with invalid year as input!");
+                      continue 'mainloop;
+                  }
+              };
+              
+              for (i, book) in books.iter_mut().enumerate() {
+                  if id == i+1 {
+                      book.edit_book(title.to_string(), author.to_string(), year);
+                      println!("Updated successfully...");
+                      continue 'mainloop;
+                  }
+              }
+              println!("No book found! Updation failed!");
+                             
+            },
+            "3" => {
                 // list books
                 for (i, book) in books.iter().enumerate() {
                     println!("{}. {}({}) - {} [{}]", i+1, book.title, book.year, book.author,
@@ -105,7 +173,7 @@ fn main() {
                     println!("No books to show");
                 }
             },
-            "3" => {
+            "4" => {
                 // Borrow book
                 println!("Enter the ID to borrow:");
                 let mut id = String::new();
@@ -127,7 +195,7 @@ fn main() {
                 
                 println!("Can't find any book with ID: {}", id);
             },
-            "4" => {
+            "5" => {
                 // Retrun book
                 println!("Enter the ID to return: ");
                 let mut id = String::new();
