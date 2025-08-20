23732efd15949cf6d11564dc69bd6b021c0ed5cc
RicardoVercet <ricardobieb01@gmail.com>
2025-08-21 03:41:42 +0530

    rust - shape calc task from traits and generics

--- Rust/traits_and_generics/src/main.rs
+++ Rust/traits_and_generics/src/main.rs
@@ -5,6 +5,7 @@ trait Shape {
 
 struct Circle { radius : f64 }
 struct Rectangle { width: f64, height: f64 }
+struct Triangle { base: f64, height: f64 }
 
 impl Shape for Circle {
     fn area(&self) -> f64 {
@@ -18,8 +19,25 @@ impl Shape for Rectangle {
     }
 }
 
-fn print_area<T: Shape>(shape: &T) {
+impl Shape for Triangle {
+    fn area(&self) -> f64 {
+        0.5 * self.base * self.height
+    }
+}
+
+fn print_area<T: Shape>(shape: &T) {    // traits and generics together, uses dynamic dispatch(runtime lookup)
     println!("Area = {}", shape.area());
+}
+
+//fn print_area_dyn(shape: &dyn Shape) {
+//    println!("Area = {}", shape.area());
+//}
+
+fn print_all_areas(l: &[Box<dyn Shape>]) {
+    for item in l {
+        //print_area_dyn(item);
+        println!("Area = {}", item.area());
+    }
 }
 
 fn main() {
@@ -27,8 +45,17 @@ fn main() {
     
     let c = Circle { radius: 3.0 };
     let r = Rectangle { width: 4.0, height: 5.0 };
+    let t = Triangle { base: 3.0, height: 6.0 };
     
-    print_area(&c);
-    print_area(&r);
+    //print_area(&c);
+    //print_area(&r);
+    
+    let mut l: Vec<Box<dyn Shape>> = Vec::new();
+    l.push(Box::new(c));
+    l.push(Box::new(r));
+    l.push(Box::new(t));
+    
+    print_all_areas(&l);
+    
     
 }
