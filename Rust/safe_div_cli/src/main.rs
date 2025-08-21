ecccdf2177c6079222c6455e27593c46f576b2e3
RicardoVercet <ricardobieb01@gmail.com>
2025-08-21 22:51:55 +0530

    rust - safe divide task brute way

--- Rust/safe_div_cli/src/main.rs
+++ Rust/safe_div_cli/src/main.rs
@@ -1,3 +1,9 @@
+// Task 
+// Take input from the user
+// handle all errors/exceptions gracefully.
+
+use std::io;
+
 fn safe_divide(a: f64, b: f64) -> Result<f64, String> {
     if b != 0.0 {
         Ok(a/b)
@@ -7,14 +13,72 @@ fn safe_divide(a: f64, b: f64) -> Result<f64, String> {
 }
 
 fn main() {
-    println!("Hello, world!");
+    println!("-- safe divide --");
+    println!("Press q to quit anytime");
     
-    let a = 4.0;
-    let b = 0.0;
-    
-    match safe_divide(a, b) {
-        Ok(data) => println!("Result: {}", data),
-        Err(e) => println!("Err: {}", e), 
+    'mainloop: loop {
+        
+        let a: f64 = 'a_loop: loop {
+            println!("Enter the first number: ");
+            let mut a = String::new();
+            if let Err(e) = io::stdin().read_line(&mut a) {
+                    println!("Input stream error occured: {}", e);
+                    break 'mainloop;
+            };
+            
+            // check if its 'q'
+            let trimmed_a = match a.trim() {
+                "q" => {
+                    println!("Goodbye...");
+                    break 'mainloop;
+                },
+                r => r,
+            };
+            
+            let a: f64 = match trimmed_a.parse() {
+                Ok(val) => val,
+                Err(_) => {
+                    println!("Please enter a valid floating point number");
+                    continue 'a_loop;
+                },
+            };
+            break 'a_loop a;
+        };
+        
+        let b: f64 = 'b_loop: loop {
+            println!("Enter the second number: ");
+            let mut b = String::new();
+            if let Err(_) = io::stdin().read_line(&mut b){
+                println!("Unable to read input from console!");
+                println!("Terminating application...");
+                break 'mainloop;
+            };
+            
+            // check if its 'q'
+            let trimmed_b = match b.trim() {
+                "q" => {
+                    println!("Goodbye...");
+                    break 'mainloop;
+                },
+                r => r,
+            };
+            let b: f64 = match trimmed_b.parse() {
+                Ok(val) => val,
+                Err(_) => {
+                    println!("Please enter a valid floating point number");
+                    continue 'b_loop;
+                },
+            };
+            break 'b_loop b;
+        };
+        
+        
+        match safe_divide(a, b) {
+            Ok(data) => println!("Result: {}", data),
+            Err(e) => println!("Err: {}", e), 
+        }
+        
+        println!("If you're not satisfied with the result, you can try again!");
     }
     
 }
