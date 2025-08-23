c5053b24a23e05da97db09879cef6102a197503e
RicardoVercet <ricardobieb01@gmail.com>
2025-08-24 00:27:55 +0530

    rust - safe divide task error handling

--- Rust/safe_div_cli/src/main.rs
+++ Rust/safe_div_cli/src/main.rs
@@ -4,6 +4,12 @@
 
 use std::io;
 
+#[derive(Debug)]
+enum ResponseTypes {
+    Io(io::Error),
+    Quit,
+}
+
 fn safe_divide(a: f64, b: f64) -> Result<f64, String> {
     if b != 0.0 {
         Ok(a/b)
@@ -12,20 +18,28 @@ fn safe_divide(a: f64, b: f64) -> Result<f64, String> {
     }
 }
 
-#[allow(dead_code)]
-fn get_input(s: &str) -> Result<f64, String> {
+fn get_input(s: &str) -> Result<f64, ResponseTypes> {
     loop {
         let mut var = String::new();
-        println!("Enter {}", s);
-        io::stdin().read_line(&mut var).unwrap();
-        let var: f64 = match var.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please enter a valid floating point decimal...");
-                continue;
-            }
+        println!("Enter {} :", s);
+        io::stdin().read_line(&mut var).map_err(ResponseTypes::Io)?;
+        let var =  match var.trim() {
+            "q" => {
+                return Err(ResponseTypes::Quit);
+            },
+            other => other,
         };
-        return Ok(var);
+        
+        let flt: f64 = match var.parse() {
+            Ok(val) => val,
+            Err(_) => {
+                println!("Please enter a valid floating point number");
+                continue;
+            },
+        };
+        
+        
+        return Ok(flt);
     }
     
 }
@@ -34,60 +48,31 @@ fn main() {
     println!("-- safe divide --");
     println!("Press q to quit anytime");
     
-    'mainloop: loop {
+    loop {
         
-        let a: f64 = 'a_loop: loop {
-            println!("Enter the first number: ");
-            let mut a = String::new();
-            if let Err(e) = io::stdin().read_line(&mut a) {
-                    println!("Input stream error occured: {}", e);
-                    break 'mainloop;
-            };
-            
-            // check if its 'q'
-            let trimmed_a = match a.trim() {
-                "q" => {
-                    println!("Goodbye...");
-                    break 'mainloop;
-                },
-                r => r,
-            };
-            
-            let a: f64 = match trimmed_a.parse() {
-                Ok(val) => val,
-                Err(_) => {
-                    println!("Please enter a valid floating point number");
-                    continue 'a_loop;
-                },
-            };
-            break 'a_loop a;
+        let a = match get_input("First Number") {
+            Ok(val) => val,
+            Err(ResponseTypes::Quit) => {
+                println!("Goodbye...");
+                break;
+            },
+            Err(ResponseTypes::Io(e)) => {
+                println!("Unable to read the stdin!");
+                println!("Err : {}", e);
+                break;
+            }
         };
         
-        let b: f64 = 'b_loop: loop {
-            println!("Enter the second number: ");
-            let mut b = String::new();
-            if let Err(_) = io::stdin().read_line(&mut b){
-                println!("Unable to read input from console!");
-                println!("Terminating application...");
-                break 'mainloop;
-            };
-            
-            // check if its 'q'
-            let trimmed_b = match b.trim() {
-                "q" => {
-                    println!("Goodbye...");
-                    break 'mainloop;
-                },
-                r => r,
-            };
-            let b: f64 = match trimmed_b.parse() {
-                Ok(val) => val,
-                Err(_) => {
-                    println!("Please enter a valid floating point number");
-                    continue 'b_loop;
-                },
-            };
-            break 'b_loop b;
+        let b = match get_input("Second Number") {
+            Ok(val) => val,
+            Err(ResponseTypes::Quit) => {
+                println!("Goodbye...");
+                break;
+            },
+            Err(_) => {
+                println!("Unable to read the stdin!");
+                break;
+            },
         };
         
         
