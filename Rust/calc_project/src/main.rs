4613e9d5c3372642531bdf9d3e7f572cfb4c049c
RicardoVercet <ricardobieb01@gmail.com>
2025-08-24 00:45:43 +0530

    rust - modules and crates task init

---
+++ Rust/calc_project/src/main.rs
@@ -0,0 +1,10 @@
+// 1. add math.rs in src/
+// 2. implement add, subtract, multiply, divide
+// 3. make them pub
+// 4. in main.rs import mod math and use these functions
+// 5. Extend it, create another file greet.rs with a function hello(name: &str)
+// 6. Call both from main.rs
+
+fn main() {
+    println!("Hello, world!");
+}
