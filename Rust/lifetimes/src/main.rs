7323f1324852efbab9096727b6015afb8ea52df9
RicardoVercet <ricardobieb01@gmail.com>
2025-08-26 22:14:37 +0530

    rust - lifetimes task

--- Rust/lifetimes/src/main.rs
+++ Rust/lifetimes/src/main.rs
@@ -1,4 +1,9 @@
 
+struct Highlight<'a> {
+    text1: &'a str,
+    text2: &'a str,
+}
+
 fn longest_word<'a>(words: &'a [&'a str]) -> &'a str {
     let mut a = 0;
     let mut b: &'a str = &"";
@@ -14,10 +19,21 @@ fn longest_word<'a>(words: &'a [&'a str]) -> &'a str {
 
 fn main() {
 
-
     let sentence = String::from("Rust is fearless and fast");
     let words: Vec<&str> = sentence.split_whitespace().collect();
 
-
     println!("Longest word : {}", longest_word(&words));
+    
+    
+    //let word = String::from("oneword");
+    //let word2 = String::from("twoword");
+    //let val = Highlight { text1: &word, text2: &word2 };
+    
+    //{    
+    //    println!("txt1 : {}", val.text1);
+    //    println!("txt2 : {}", val.text2);
+    //}
+    
+    //println!("{} | {}", val.text1, val.text2);
+    
 }
