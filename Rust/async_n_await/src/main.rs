c41682e5ecd19a5929620f07c1100753838ae895
RicardoVercet <ricardobieb01@gmail.com>
2025-08-27 16:20:50 +0530

    rust - run multiple async functions

--- Rust/async_n_await/src/main.rs
+++ Rust/async_n_await/src/main.rs
@@ -2,13 +2,30 @@ use tokio::time::{sleep, Duration};
 
 #[tokio::main]
 async fn main() {
-    basic_example().await;
+    // basic_example().await;
+    tokio::join!(task1(), task2());
 }
 
+async fn task1() {
+    for i in 1..=8 {
+        println!("Task 1: {}", i);
+        sleep(Duration::from_millis(500)).await;
+    }
+}
+
+async fn task2() {
+    for i in 1..=5 {
+        println!("Task 2: {}", i);
+        sleep(Duration::from_millis(700)).await;
+    }
+}
+
+#[allow(dead_code)]
 async fn basic_example() {
     say_hello().await;
 }
 
+#[allow(dead_code)]
 async fn say_hello() {
     println!("Hello...");
     sleep(Duration::from_secs(1)).await;
