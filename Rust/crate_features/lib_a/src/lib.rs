99be9ab97400d0408468acb2f8ed95098cef0f70
RicardoVercet <ricardobieb01@gmail.com>
2025-09-01 23:54:22 +0530

    rust - import features

---
+++ Rust/crate_features/lib_a/src/lib.rs
@@ -0,0 +1,21 @@
+
+#[cfg(feature="feature-add")]
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(feature="feature-sub")]
+pub fn sub(left: u64, right: u64) -> u64 {
+    left - right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
