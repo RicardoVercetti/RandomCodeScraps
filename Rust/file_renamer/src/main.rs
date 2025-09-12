d09b7351ace8b7e6bd8529500a64c444b0a866ae
RicardoVercet <ricardobieb01@gmail.com>
2025-09-12 20:25:41 +0530

    rust - file renamer

---
+++ Rust/file_renamer/src/main.rs
@@ -0,0 +1,12 @@
+// file reneamer
+
+// 1. take input for directory location
+// 2. take input for pattern
+// 3. get all the files from the directory
+// 4. rename one by one with pattern and numbers incrementally(eg: vacation-001.jpg, vacation-002.jpg, etc.,)
+
+fn main() {
+    println!("-- Welcome to file renamer --");
+
+    
+}
