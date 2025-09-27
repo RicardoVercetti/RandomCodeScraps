05045abc698d989831a9d357c686fdd4ac92d778
RicardoVercet <ricardobieb01@gmail.com>
2025-09-28 00:50:19 +0530

    rust - shell program

---
+++ Rust/rust_shell/src/main.rs
@@ -0,0 +1,15 @@
+// a shell taked stdin for itself and gives the same to other programs too
+// shell also takes stdout form other programs that it runs
+// cd is built into the shell's current process
+// ctrl + c should send the sigint to the process that the shell is running
+
+
+// other notes
+
+// things like stdin and stdout have to be already built
+// calling commands that are available in the operating system bin should be there
+
+
+fn main() {
+    println!("Hello, world!");
+}
