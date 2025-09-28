4551d4b06c88b3ad63498408be8944a1e96d8e59
RicardoVercet <ricardobieb01@gmail.com>
2025-09-28 14:11:21 +0530

    rust - shell with command and loop

--- Rust/rust_shell/src/main.rs
+++ Rust/rust_shell/src/main.rs
@@ -9,7 +9,31 @@
 // things like stdin and stdout have to be already built
 // calling commands that are available in the operating system bin should be there
 
+use std::io::{ stdin, stdout, Write };
+use std::process::Command;
 
 fn main() {
-    println!("Hello, world!");
+    
+    loop{
+    // use the `>` charecter as the prompt
+    // needs to explicitly flush this to ensure it prints before read_line
+    print!("> ");
+    stdout().flush();
+    
+    let mut input = String::new();
+    
+    stdin().read_line(&mut input).unwrap();
+    
+    let mut parts = input.trim().split_whitespace();
+    let command = parts.next().unwrap();
+    let args = parts;
+    
+    
+    let mut child = Command::new(command)
+            .args(args)
+            .spawn()
+            .unwrap();
+    
+    child.wait();
+    }
 }
