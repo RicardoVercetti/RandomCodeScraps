dda9322b6fa74f6a35a27f4dbf8d0104d7f6f620
RicardoVercet <ricardobieb01@gmail.com>
2025-11-23 01:22:14 +0530

    rust - format rusppims

--- Rust/rusppims/src/main.rs
+++ Rust/rusppims/src/main.rs
@@ -4,6 +4,7 @@ use axum::{
     Router,
 };
 use serde::{Deserialize, Serialize};
+use tokio::net::TcpListener;
 
 #[derive(Serialize, Deserialize)]
 struct MyData {
@@ -24,19 +25,29 @@ struct PingPostResponse {
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "PPIMS here, howdy partner!" }).post(ping_post));
+    let app: Router = Router::new().route(
+        "/",
+        get(ping_get).post(ping_post),
+    );
 
     // run our app with hyper, listening globally on port 3000
-    let port = "0.0.0.0:3000";
-    let listener = tokio::net::TcpListener::bind(port).await.unwrap();
+    let port: &str = "0.0.0.0:3000";
+    let listener: TcpListener = tokio::net::TcpListener::bind(port).await.unwrap();
+
+    let server = axum::serve(listener, app);
     println!("Running http server on :{port}");
-    axum::serve(listener, app).await.unwrap();
-    
+    server.await.unwrap();
+}
+
+async fn ping_get() -> String {
+    "Rusppims here, howdy partner!".to_string()
 }
 
 async fn ping_post(Json(payload): Json<PingPostData>) -> Json<PingPostResponse> {
     println!("req: {:?}", payload);
+    // you can do something with the data ya know!
 
-
-    Json(PingPostResponse { message: "Requst received successfully".to_string() })
+    Json(PingPostResponse {
+        message: "Requst received successfully".to_string(),
+    })
 }
