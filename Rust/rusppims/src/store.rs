2bc1e17a8269db1d7c86bf02ed2b279a33cb6412
RicardoVercet <ricardobieb01@gmail.com>
2025-11-23 18:34:07 +0530

    rust - rusppims deserialize customer info from string

--- Rust/rusppims/src/store.rs
+++ Rust/rusppims/src/store.rs
@@ -1,22 +1,40 @@
-use tokio::{
-    fs::{ 
-        File, 
-        metadata, 
-        read_to_string 
-    }, 
-    io::AsyncWriteExt
+use serde::{
+    Deserialize, 
+    Serialize
 };
 use std::io::Error;
+use tokio::{
+    fs::{File, metadata, read_to_string},
+    io::AsyncWriteExt,
+};
 
-// define customer info here
 
-// define add customer here
+#[derive(Serialize, Deserialize, Debug)]
+struct CustomerInfo {
+    unique_id: String, // this is the ppid
+    maiden_name: String,
+    mobile_number: String,
+    date_of_birth: String,
+    account_number: String,
+    account_status: String,
+    card_number: String,
+    card_exp_date: String,
+    card_status: String,
+    kyc_flag: String,
+    kyc_updated_channel: String,
+    kyc_updated_on: String,
+    ovid_value: String,
+    ovid_type: String,
+    cif_id: String,
+}
 
-// define get cusomer info here
+// TODO: define add customer here
 
-// define write to file here
+// TODO: define get cusomer info here
 
-// load file/create file
+// TODO: define write to file here
+
+// TODO: load file/create file
 async fn load_or_create_file() -> Result<String, Error> {
     let filename = "customers.json";
     if metadata(filename).await.is_ok() {
@@ -27,13 +45,16 @@ async fn load_or_create_file() -> Result<String, Error> {
     // file doesn't exist, create one
     println!("customers.json is not found, initiating one...");
     let mut file = File::create(filename).await?;
-    file.write_all(b"{}").await?;
+    file.write_all(b"[]").await?;
     
-    return Ok("{}".to_string());
+    return Ok("[]".to_string());
 }
 
 // deserialize customer info from the customer.json string
-
+fn deserialize_json_string(str: &str) -> Result<Vec<CustomerInfo>, serde_json::Error> {
+    let customers_info: Vec<CustomerInfo> = serde_json::from_str(&str)?;
+    Ok(customers_info)
+}
 
 #[cfg(test)]
 mod tests {
