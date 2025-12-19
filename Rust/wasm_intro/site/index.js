8d221dd5b8f73c4dcec01552e556e426b8490bc1
RicardoVercet <ricardobieb01@gmail.com>
2025-12-19 23:01:58 +0530

    rust - js package with wasm

---
+++ Rust/wasm_intro/site/index.js
@@ -0,0 +1,3 @@
+import * as wasm from "wasm_intro";
+
+wasm.greet("WebAssembly with npm");
--- Rust/wasm_intro/src/lib.rs
+++ Rust/wasm_intro/src/lib.rs
@@ -10,3 +10,7 @@ pub fn greet(name: &str) {
     
     alert(&format!("Hello, {}!", name));
 }
+
+// commands
+// wasm-pack build --target web                 →   compile straight for web
+// wasm-pack build --target bundler             →   as a package available to npm
