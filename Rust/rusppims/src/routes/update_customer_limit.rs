6e124a0897006083b8df91aab9334f1558d2cadb
RicardoVercet <ricardobieb01@gmail.com>
2025-12-28 16:47:52 +0530

    rust - rusppims update customer limit logic

--- Rust/rusppims/src/routes/update_customer_limit.rs
+++ Rust/rusppims/src/routes/update_customer_limit.rs
@@ -4,7 +4,7 @@ use axum::{Json, extract::State};
 use serde::{Deserialize, Serialize};
 use tokio::sync::RwLock;
 
-use crate::{store::CustomerInfo, utils::print_req_res};
+use crate::{store::{CustomerInfo, save_to_file}, utils::{find_n_get_mut_customer_info, print_req_res}};
 pub async fn handle_update_customer_limit(
     State(_state): State<Arc<RwLock<Vec<CustomerInfo>>>>,
     Json(payload): Json<UpdateCustomerLimitRequest>
@@ -12,7 +12,44 @@ pub async fn handle_update_customer_limit(
     
     print_req_res(&payload, "Req");
 
-    let res = UpdateCustomerLimitResponse::new("500".to_string(), "NA".to_string());
+    // update the 'consumed' of CustomerInfo to consumed + amount(from request)
+    // assumptions:
+    // we don't check the limit here, its checked at check customer limit call
+    // always Debit, credit will be added later
+    // assume that the transaction status from the request is always 'S'(for success), I don't know the scenario where it would be 'F' and what to do then.
+
+    let mut locked_all_customers = _state.write().await;
+
+    let customer_info = find_n_get_mut_customer_info(&mut locked_all_customers, &payload.data.update_customer.unique_id);
+
+    let res = match customer_info {
+        Some(cus) => {
+            // update the limit
+
+            let amount_to_consume: Result<f32, std::num::ParseFloatError> = payload.data.update_customer.amount.parse::<f32>();
+            match amount_to_consume {     // parsing into float 32 failed
+                Err(err) => {
+                    println!("{}", err);
+                    UpdateCustomerLimitResponse::new("400".to_string(), cus.unique_id.to_string())
+                },
+                Ok(amt) => {
+                    // here update the consumed amount
+                    cus.consumed += amt;
+                    let unique_id = cus.unique_id.to_string();
+                    drop(locked_all_customers);
+                    let read_only_all_customers = _state.read().await;
+
+                    let save_status = save_to_file(&read_only_all_customers).await;
+                    if let Err(err) = save_status {
+                        print!("saving to file failed: {}", err);
+                    };
+
+                    UpdateCustomerLimitResponse::new("200".to_string(), unique_id)
+                }
+            }
+        },
+        None => UpdateCustomerLimitResponse::new("500".to_string(), "NA".to_string()),
+    };
 
     print_req_res(&res, "Res");
     Json(res)
