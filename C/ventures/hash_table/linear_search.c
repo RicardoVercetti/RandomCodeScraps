491f597aa0c2974ffcc7d8a8111b727f8e487201
RicardoVercet <ricardobieb01@gmail.com>
2026-01-28 23:03:20 +0530

    C - fix compile errors in linear_search.c

--- C/ventures/hash_table/linear_search.c
+++ C/ventures/hash_table/linear_search.c
@@ -14,10 +14,10 @@ void display_item_array(item *items, size_t size) { // its always a single point
 }
 
 item* linear_search(item* items, size_t size, char* key) {  // here char* is used instead of char[] cuz any array is passed as a pointer to first element, just that char array is null terminated.
-    for(int i=0; i<size, i++) {
+    for(int i=0; i<size; i++) {
         printf("item being compared: %s\n", items[i].key);
 
-        int stat = strcomp(items[i].key, key);
+        int stat = strcmp(items[i].key, key);
         if(stat == 0) {
             printf("the value is equal: %s\n", items[i].key);
             return &items[i];
@@ -64,7 +64,7 @@ int main() {
 
     size_t size = sizeof(items) / sizeof(item);
 
-    printf("size: %d\n", size);
+    printf("size: %zu\n", size);
 
     // display_item_array(items, size);
     item* found_item = linear_search(items, size, "jane");
@@ -73,7 +73,7 @@ int main() {
         printf("item not found!\n");
     } else {
         printf("item found!\n");
-        printf("key: %s, value: %d\n", *found_item.key, *found_item.value);
+        printf("key: %s, value: %d\n", found_item->key, found_item->value);
     }
 
     return 0;
