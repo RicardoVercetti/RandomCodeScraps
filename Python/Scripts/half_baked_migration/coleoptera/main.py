e74c14720cea5b1b4776a47d2327cc3da291276c
RicardoVercet <ricardobieb01@gmail.com>
2026-03-26 16:26:01 +0530

    pyscript - add filemetadata in status

--- Python/Scripts/half_baked_migration/coleoptera/main.py
+++ Python/Scripts/half_baked_migration/coleoptera/main.py
@@ -5,33 +5,36 @@ Main file that is the entry point of the migration CLI logic
 import traceback
 from src.utils.logger_setup import logger
 from src.utils.utils import get_and_classify_files
-from src.sanitycheck.sanity_check import sanity_check_cards, sanity_check_accounts, sanity_check_for_customers
+from src.sanitycheck.sanity_check import sanity_check_cards, sanity_check_accounts, sanity_check_for_customers, ProgressStatus
 
 def main() -> None:
     logger.info("Hello from coleoptera!")
     logger.info("here is the first line of logging...")
     classified: dict[str, list[str]] = get_and_classify_files()
+
+
+    # todo: #1 check if the config already exists, else do the config run
+    
+    # todo: #2 else do the migration run 
+
+    status_lists: list[ProgressStatus] = []
+
     logger.info("Classified items:")
     for index, (key, values) in enumerate(classified.items()):
         logger.info("%d %s : %s", index + 1, key, values)
 
         if key == "cards":
             progress = sanity_check_cards(values)
-            # print(f"progress: {progress}")
-            if progress.failed_items > 0:
-                logger.info("there are %d errors found in the %s file", progress.failed_items, progress.source_name)
+            status_lists.append(progress)
         elif key == "accounts":
             progress = sanity_check_accounts(values)
-
-            if progress.failed_items > 0:
-                logger.info("there are about %d errors found in the %s file", progress.failed_items, progress.source_name)
+            status_lists.append(progress)
         elif key == "customers":
             progress = sanity_check_for_customers(values)
-
-            if progress.failed_items > 0:
-                logger.info(f"there are about {progress.failed_items} errors found in the {progress.source_name} file")
+            status_lists.append(progress)
         else:
             logger.info("skipping for %s", key)
+    print("all finished!")
 
 
 if __name__ == "__main__":
--- Python/Scripts/half_baked_migration/coleoptera/src/sanitycheck/sanity_check.py
+++ Python/Scripts/half_baked_migration/coleoptera/src/sanitycheck/sanity_check.py
@@ -7,11 +7,21 @@
 from dataclasses import dataclass
 from src.sanitycheck.helpers import remove_double_quotes, is_all_digits, is_empty
 from src.utils.logger_setup import logger
+from typing import Optional
 
 @dataclass
 class CardProductDto:
     name: str
     bin: str
+    is_dulplicated: bool
+
+    def to_dict(self) -> dict[str, str]:
+        return_dict = {
+            "name": self.name,
+            "bin": self.bin,
+            "is_duplicated": self.is_dulplicated
+        }
+        return return_dict
 
 class InvalidCardProgram(Exception):
     pass
@@ -37,12 +47,27 @@ class CardProducts:
         return self.card_products
 
 @dataclass
+class FileMetaData:
+    filename: str
+    is_corrupt: bool
+    no_of_items: int
+
+    def to_dict(self) -> dict[str, str]:
+        return_dict = {
+            "filename": self.filename,
+            "is_corrupt": self.is_corrupt,
+            "no_of_items": self.no_of_items
+        }
+        return return_dict
+
+@dataclass
 class ProgressStatus:
     source_name: str
     total_items: int
     failed_items: int
-    # errors: list[str]           # have information on line number, filename, and perhaps even the card/account/customer number as well
+    # errors: list[str]           # have information on line number, filename, and perhaps even the card/account/customer number as well :update: this might become too lengthy of a data in memory file size is too large 
     card_product_list: list[CardProductDto]
+    filemetadata: list[FileMetaData]
 
 
 # cards load file
@@ -63,76 +88,85 @@ def sanity_check_cards(filenames: list[str]) -> ProgressStatus:
     # i. go line by line and check the above
     # ii. add the card program list to conf
     # iii. take bin no configuration for each card product
-    status = ProgressStatus("cards", None, 0, card_product_list=[])
+    status = ProgressStatus("cards", None, 0, card_product_list=[], filemetadata=[])
     critical_error = False                  # if this occurs, we should'nt insert any data
     total_lines = 0
     card_products = CardProducts()
 
     # then call it a day
     for filename in filenames:
+        file_metadata = FileMetaData(filename, False, 0)
         lines = 0           # per file line count
-        with open(filename, "r") as file:
-            logger.info(f"performing cards extract sanity check on file: {filename}")
-            for line in file:                               # note each line has a '\n' at the end of the file
-                # now start checking rules by each line
-                oneline = line.strip().split(",")
-                lines += 1                                  # current line
-                
-                if critical_error:      # skipping so that we can get the line number count
-                    status.failed_items += 1
-                    continue
+        try:
+            with open(filename, "r") as file:
+                logger.info(f"performing cards extract sanity check on file: {filename}")
+                for line in file:                               # note each line has a '\n' at the end of the file
+                    # now start checking rules by each line
+                    oneline = line.strip().split(",")
+                    lines += 1                                  # current line
+                    
+                    if critical_error:      # skipping so that we can get the line number count
+                        status.failed_items += 1
+                        continue
 
-                # totally 34 items in per row of cards.txt
-                if len(oneline) != 34:
-                    critical_error = True
-                    # status.errors.append(f"Invalid length: {len(oneline)} in line {lines} of file: {filename}. Lenth must be exactly {34}")
-                    status.failed_items += 1
-                    logger.error(f"Invalid length: {len(oneline)} in line {lines} of file: {filename}. Lenth must be exactly {34}")
-                    continue
+                    # totally 34 items in per row of cards.txt
+                    if len(oneline) != 34:
+                        critical_error = True
+                        # status.errors.append(f"Invalid length: {len(oneline)} in line {lines} of file: {filename}. Lenth must be exactly {34}")
+                        status.failed_items += 1
+                        logger.error(f"Invalid length: {len(oneline)} in line {lines} of file: {filename}. Lenth must be exactly {34}")
+                        continue
 
-                # format data so that the double quotes can be removed
-                clean_pan = remove_double_quotes(oneline[0])
-                
-                # 16 length logics are the only things handled, if else the bin stripping logic changes
-                if len(clean_pan) != 16:
-                    # status.errors.append(f"Invalid pan length: '{oneline[0]}', in line {lines} of file: {filename}")
-                    status.failed_items += 1
-                    logger.error(f"Invalid pan length: '{len(clean_pan)}', in line {lines} of file: {filename}")
-                
-                # pan should be all numeric
-                if not is_all_digits(clean_pan):
-                    status.failed_items += 1
-                    logger.error(f"pan number must be all numeric. Got: '{clean_pan}' at line: {lines} of file: {filename}")
+                    # format data so that the double quotes can be removed
+                    clean_pan = remove_double_quotes(oneline[0])
+                    
+                    # 16 length logics are the only things handled, if else the bin stripping logic changes
+                    if len(clean_pan) != 16:
+                        # status.errors.append(f"Invalid pan length: '{oneline[0]}', in line {lines} of file: {filename}")
+                        status.failed_items += 1
+                        logger.error(f"Invalid pan length: '{len(clean_pan)}', in line {lines} of file: {filename}")
+                    
+                    # pan should be all numeric
+                    if not is_all_digits(clean_pan):
+                        status.failed_items += 1
+                        logger.error(f"pan number must be all numeric. Got: '{clean_pan}' at line: {lines} of file: {filename}")
 
-                # if card product name is same then bin should be same for each card
-                clean_product_name = remove_double_quotes(oneline[2])
-                try:
-                    card_products.check_and_push(clean_pan, clean_product_name)
-                except InvalidCardProgram as e:
-                    # status.errors.append(f"{e}")
-                    status.failed_items += 1
-                    logger.error(f"{e}")
+                    # if card product name is same then bin should be same for each card
+                    clean_product_name = remove_double_quotes(oneline[2])
+                    try:
+                        card_products.check_and_push(clean_pan, clean_product_name)
+                    except InvalidCardProgram as e:
+                        # status.errors.append(f"{e}")
+                        status.failed_items += 1
+                        logger.error(f"{e}")
 
-                # last updated date cannot be empty
-                clean_last_updated_date = remove_double_quotes(oneline[32])
-                if len(clean_last_updated_date) < 1:
-                    # status.errors.append(f"last updated date is invalid at line: {lines} of file: {filename}. Value found: '{oneline[32]}'")
-                    status.failed_items += 1
-                    logger.error(f"last updated date is invalid at line: {lines} of file: {filename}. Value found: '{clean_last_updated_date}'")
+                    # last updated date cannot be empty
+                    clean_last_updated_date = remove_double_quotes(oneline[32])
+                    if len(clean_last_updated_date) < 1:
+                        # status.errors.append(f"last updated date is invalid at line: {lines} of file: {filename}. Value found: '{oneline[32]}'")
+                        status.failed_items += 1
+                        logger.error(f"last updated date is invalid at line: {lines} of file: {filename}. Value found: '{clean_last_updated_date}'")
 
-                # last updated user cannot be empty
-                clean_last_updated_user = remove_double_quotes(oneline[33])
-                if len(clean_last_updated_user) < 1:
-                    # status.error.append(f"last updated user is invalid at line: {lines} of file: {filename}. Value found: '{oneline[33]}'")
-                    status.failed_items += 1
-                    logger.error(f"last updated user is invalid at line: {lines} of file: {filename}. Value found: '{clean_last_updated_user}'")
-            logger.info(f"fininshed sanity check on file: {filename}")
+                    # last updated user cannot be empty
+                    clean_last_updated_user = remove_double_quotes(oneline[33])
+                    if len(clean_last_updated_user) < 1:
+                        # status.error.append(f"last updated user is invalid at line: {lines} of file: {filename}. Value found: '{oneline[33]}'")
+                        status.failed_items += 1
+                        logger.error(f"last updated user is invalid at line: {lines} of file: {filename}. Value found: '{clean_last_updated_user}'")
+                logger.info(f"fininshed sanity check on file: {filename}")
 
-        total_lines += lines      
+            # End with
+        except FileNotFoundError as e:
+            logger.info(f"file not found error: {e}")
+        except Exception as f:
+            logger.error(f"exception occured: {f}, marking file: {filename} as corrupt")
+            file_metadata.is_corrupt = True
+        file_metadata.no_of_items = lines
+        status.filemetadata.append(file_metadata)
     # main fn
     status.total_items = total_lines
     status.card_product_list = card_products.get_all_products()
-    return status     
+    return status
                 
 
 # accounts load file
@@ -144,13 +178,14 @@ def sanity_check_accounts(filenames: list[str]):
     # 8. last_updated_date
     # 9. last_updated_user
 
-    status = ProgressStatus("accounts", 0, 0, [])
+    status = ProgressStatus("accounts", 0, 0, [], [])
     critical_error = False
     total_lines = 0
 
     for accounts_file in filenames:
         logger.info(f"started sanity check for accounts file: {accounts_file}")
         lines = 0
+        file_metadata = FileMetaData(accounts_file, False, 0)
         with open(accounts_file, "r") as file:
 
             for line in file:
@@ -206,6 +241,11 @@ def sanity_check_accounts(filenames: list[str]):
                     status.falied_items += 1
                     logger.error(f"Invalied last updated user date at line: {lines} of file: {accounts_file}")
         
+        # todo: do a try catch for each files open
+
+        # metadata updates
+        file_metadata.no_of_items = lines
+        status.filemetadata.append(file_metadata)
         total_lines += lines
         logger.info(f"finised sanity check for account file: {accounts_file}")
     # main fn
@@ -223,13 +263,14 @@ def sanity_check_for_customers(filenames: list[str]) -> ProgressStatus:
     # 36: vip_flag
     # 39: last_updated_date
     # 40: last_updated_user
-    status = ProgressStatus("customers", 0, 0, [])
+    status = ProgressStatus("customers", 0, 0, [], [])
     total_items = 0
     critical_error = False
 
     for filename in filenames:
         logger.info(f"started sanity check for customers file: {filename}")
         line_nr = 0
+        file_metadata = FileMetaData(filename, False, 0)
         try:
             with open(filename, "r") as file:
                 for line in file:
@@ -286,7 +327,10 @@ def sanity_check_for_customers(filenames: list[str]) -> ProgressStatus:
             logger.error(f"decode error for file: {filename} at line: {line_nr}, skipping sanity check for this file")
             status.failed_items += 1
             logger.error(err)
+            file_metadata.is_corrupt = True
 
+        file_metadata.no_of_items = line_nr
+        status.filemetadata.append(file_metadata)
         total_items += line_nr
         logger.info(f"finished santiy check for customers file: {filename}")
     # end for
