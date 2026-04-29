1bb7950200831192e67ad468db3d6ef2d6da4e9f
RicardoVercet <ricardobieb01@gmail.com>
2026-04-29 22:23:06 +0530

    py - games/intro: extras 1, 2

--- Python/games/intro/README.md
+++ Python/games/intro/README.md
@@ -1,2 +1,12 @@
 ## list
-1. linter for python in vscodium?
+
+###  DEV env basics
+- [X] linter for python in vscodium
+- [ ] code analyzer extension in vscodium
+
+### Tasks
+- [X] Sample from official documentation
+- [ ] Breakout game with simple rectangles
+
+### Problems
+- when some circles reach the bottom or top, they don't really bounce back, they're kinda stuck at the top or bottom with horizontal momentum
--- Python/games/intro/main.py
+++ Python/games/intro/main.py
@@ -14,18 +14,18 @@ running = True
 # task
 # draw a circle that will auto hit the edges and get bounced back in the opposite direction
 # requirements:
-# 1. being able to put a circle
-# 2. circle has a width? starting position in the screen/canvas
-# 3. has a momentum to one direction
-# 4. when the edge of the circle touches(equal to or greater than the edges), the momentum reverses
-# 5. this repeats without any external input
+# 1. [OK] being able to put a circle
+# 2. [OK] circle has a width? starting position in the screen/canvas
+# 3. [OK] has a momentum to one direction
+# 4. [OK] when the edge of the circle touches(equal to or greater than the edges), the momentum reverses
+# 5. [OK] this repeats without any external input
 
 # extras
-# 1. spawn in random with random size
-# 2. spawn several other loons and make them move in random
-# 3. when they collide with each other, they should bounce opposite?
-# 4. smaller ones get more momentum when hitting against bigger ones
-# 5. try acceleration part soon after they hit against one another, then return to their constant speeds
+# 1. [OK] spawn in random with random size
+# 2. [OK] spawn several other loons and make them move in random
+# 3. [ ] when they collide with each other, they should bounce opposite?
+# 4. [ ] smaller ones get more momentum when hitting against bigger ones
+# 5. [ ] try acceleration part soon after they hit against one another, then return to their constant speeds
 
 
 def random_position_in_screen():
@@ -49,32 +49,29 @@ class Circle:
         self.size = size
 
     def update_position(self):
+        # change direction if we hit the corners
         half_size = self.size/2
         if (self.x_pos + half_size) >= WIDTH or (self.x_pos - half_size) <= 0:
             self.x_dir *= -1
         if (self.y_pos + half_size) >= HEIGHT or (self.y_pos - half_size) <= 0:
             self.y_dir *= -1
 
+        # movements
         self.x_pos += self.x_dir
         self.y_pos += self.y_dir
 
     @staticmethod
     def random_pos():
-        speed = 10
+        speed = random.randrange(7, 12)
         x_dir = -(speed) if random.randrange(6) % 2 == 0 else speed
         y_dir = -(speed) if random.randrange(6) % 2 == 0 else speed
         # size = random.randrange(100, 120)
-        size = 100
+        size = random.randrange(40, 100)
         return Circle(random.randrange(WIDTH), random.randrange(HEIGHT), x_dir, y_dir, size)
 
 player_pos = pygame.Vector2(WIDTH/2, HEIGHT/2)
 # last_pressed = time.time()
-one_circle = Circle.random_pos()
-
-
-# def is_time_since_last_pressed():
-#     now = time.time()
-#     return now - last_pressed > 0.1 
+list_of_circle = [Circle.random_pos() for i in range(10)]
 
 while running:
     
@@ -89,10 +86,11 @@ while running:
     # screen filling first
     screen.fill("purple")        # maybe try some other color next time
 
-    one_circle.update_position()
-
-    # draw the content
-    pygame.draw.circle(screen, "red", pygame.Vector2(one_circle.x_pos, one_circle.y_pos), one_circle.size)      # so the last item is the size
+    for circle in list_of_circle:
+        circle.update_position()
+        # draw the content
+        pygame.draw.circle(screen, "red", pygame.Vector2(circle.x_pos, circle.y_pos), circle.size)
+    
     pygame.display.flip()
     clock.tick(60)
 
