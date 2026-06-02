18fcfbc5dafbc4be9d4ba2b3a20bd7ccda4a8d4c
RicardoVercet <ricardobieb01@gmail.com>
2026-06-02 23:35:00 +0530

    rust - tarball works

--- Rust/test_ground/tokyooo/src/main.rs
+++ Rust/test_ground/tokyooo/src/main.rs
@@ -7,23 +7,60 @@
 // 6. use sha256 to calculate the hash and [u8] → hex string
 // 7. try a closure function that will have different methods to extract content from the checksum URL
 
+use futures::TryStreamExt;
+use tokio_util::compat::FuturesAsyncReadCompatExt;
+
+
+fn get_filename_from_url(full_url: &String) -> Option<String> {
+    // get the string after the last '/'
+    let pos = full_url.rfind("/");
+    if let Some(position_value) = pos {
+        let subbed = full_url.get((position_value + 1)..full_url.len()).expect("sub-stringing failed!");
+
+        // find the first '.' and return all the strings after that
+        let op_ext = subbed.find(".");
+
+        if let Some(ext_pos) = op_ext {
+            let opt_ext = subbed.get(ext_pos..subbed.len());
+            if let Some(ext) = opt_ext {
+                return Some(ext.to_string());
+            }
+        }
+    }
+    None
+}
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello omniverse...");
 
-    let response = reqwest::get("https://releases.astral.sh/github/uv/releases/download/0.11.17/uv-i686-unknown-linux-gnu.tar.gz")
+    let full_url = String::from("https://releases.astral.sh/github/uv/releases/download/0.11.17/uv-i686-unknown-linux-gnu.tar.gz");
+
+    let filename = get_filename_from_url(&full_url);
+
+    if let Some(fname) = filename {
+        println!("filename is: {}", fname);
+    } else {
+        println!("Function did not return a filename");
+    }
+
+    let response = reqwest::get(&full_url)
     .await?;
 
-    let response_headers = response.headers().clone();
-    let _response_bytes = response.bytes().await?;
+    let _response_headers = response.headers().clone();      // if the file name is not found from the URL, gotta rely on the names in the header
+    // let _response_bytes = response.bytes().await?;
 
-    // let response_str: String = response_bytes.iter().map(|b| format!("{:02x}", b)).collect();
+    let tarball = response
+        .bytes_stream()
+        .map_err(std::io::Error::other)
+        .into_async_read()
+        .compat();
+
     
-    // println!("response bytes: {}", response_str);
-    for (header_name, header_value) in response_headers {
-        println!("headerName: {:?}, headerValue: {:?}", header_name.unwrap(), header_value);
-    }
+
+
+
 
 
     Ok(())
