a4b7c599d25fcb1a75da9cb941ce9f2b1319fc2d
RicardoVercet <ricardobieb01@gmail.com>
2026-06-29 22:52:14 +0530

    rust - sneak peek into infix-prefix-postfix

--- Rust/shieber/src/main.rs
+++ Rust/shieber/src/main.rs
@@ -130,6 +130,11 @@ fn decimal_conversions() {
 
 fn main() {
     println!("helo, russians?");
-
-    decimal_conversions();
+    
+    // infix to prefix and postfix
+    // B O D M A S
+    // a + b * c            => (a + (b * c))                    => +a*bc
+    // a * (b + c) / e + f  => (((a * (b + c)) / e) + f)        => +/*a+bcef
+    // (A + B) * C - (D + E) / (F + G) => ((A + B) * C) - ((D + E)/(F + G)) => -*+ABC/+DE+FG
+    
 }
