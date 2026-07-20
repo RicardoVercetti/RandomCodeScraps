15634272300b23098da1a20a35c15d658558c341
RicardoVercet <ricardobieb01@gmail.com>
2026-07-21 00:01:21 +0530

    jav - some theory

--- Java/JavaInternals/JavaInternals/src/main/java/org/example/ListsAndArrays.java
+++ Java/JavaInternals/JavaInternals/src/main/java/org/example/ListsAndArrays.java
@@ -1,29 +1,26 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class ListsAndArrays {           //  https://www.geeksforgeeks.org/java/java-collection-tutorial/
     // notes:
     // 1. dangling else ambiguity - every inner if is paired up with every nearest below else statement
+    // 2. ArrayList implementation - add(by index, element), remove(index, element), get(index), indexOf(element), addAll, removeAll, size, clear + some iterators
+    // 3.
     public static void main(String[] args) {
         System.out.println("started yooo....");
 
-        int a = 2;
-        int b = 3;
+        ArrayList<Integer> someArray = new ArrayList<Integer>();
+        for(int i=1; i<=20; i++) {
+            someArray.add(i);
+//            if (i%5==0) someArray.remove(Integer.valueOf(i-1));
+        }
 
-        if (a==2) if (b==3) System.out.println("ininside");
-        else System.out.println("lA neighbourhood");
-        else System.out.println("one more...");
+        someArray.add(1, Integer.valueOf(30));
+        someArray.remove(2);
 
-        System.out.println("outside of any of this...");
 
-//        int n = 0;
-//        int m = 0;
-//        boolean haveToRun = true;
-//        while(haveToRun) {
-//            if (n == 5) if (m == 5) haveToRun = false;
-//            else System.out.println("not true I guess");
-//            n++;
-//            m++;
-//            System.out.println("continuting to increment");
-//        }
+        System.out.println("array data: " + someArray);
+        System.out.println("size: " + someArray.size());
     }
 }
