283473ef185311c763bb4861e12404e4942d1855
RicardoVercet <ricardobieb01@gmail.com>
2026-09-02 21:59:19 +0530

    jav - neetcode: Trees-6

--- Java/JavaInternals/JavaInternals/src/main/java/org/example/trees/SubTreeOfAnotherTree.java
+++ Java/JavaInternals/JavaInternals/src/main/java/org/example/trees/SubTreeOfAnotherTree.java
@@ -36,5 +36,50 @@ public class SubTreeOfAnotherTree {
 
     public static void main(String[] args) {
         System.out.println("here goes nothing...");
+
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t5 = new TreeNode(5);
+        TreeNode t6 = new TreeNode(6);
+
+        t1.left = t2;
+        t1.right = t3;
+        t2.left = t4;
+        t2.right = t5;
+        t4.left = t6;
+
+        TreeNode s1 = new TreeNode(2);
+        TreeNode s2 = new TreeNode(4);
+        TreeNode s3 = new TreeNode(5);
+        s1.left = s2;
+        s1.right = s3;
+
+        boolean result = isSubTreeOfAnotherTree(t1, s1);
+        System.out.println("result: " + result);
+    }
+
+    public static boolean isSubTreeOfAnotherTree(TreeNode root, TreeNode subroot) {
+        if (root == null) {
+            return false;
+        }
+
+        boolean current = isCurrentTreeMatch(root, subroot);
+        boolean left = isCurrentTreeMatch(root.left, subroot);
+        boolean right = isCurrentTreeMatch(root.right, subroot);
+        return current || left || right;
+    }
+
+    public static boolean isCurrentTreeMatch(TreeNode root, TreeNode subroot) {
+        if (root == null && subroot == null) return true;
+        if (root == null || subroot == null) return false;
+
+        if (root.val == subroot.val) {
+            boolean left = isCurrentTreeMatch(root.left, subroot.left);
+            boolean right = isCurrentTreeMatch(root.right, subroot.right);
+            return left && right;
+        }
+        return false;
     }
 }
