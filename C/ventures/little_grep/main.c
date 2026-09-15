43ce816a193c8d850d6a26612e901e63e5821d5a
RicardoVercet <ricardobieb01@gmail.com>
2026-09-15 22:30:19 +0530

    c - grep from s-C-ratch

---
+++ C/ventures/little_grep/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("application runs though...\n");
+    printf("size: %d\n", argc);
+    printf("value1: %s\n", argv[0]);
+    printf("value2: %s\n", argv[1]);
+}
