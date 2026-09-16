36b1ee62c5382e1aa5b11a2ee604779c4c63f459
RicardoVercet <ricardobieb01@gmail.com>
2026-09-16 08:36:33 +0530

    c - stdin for grep from s-C-ratch

--- C/ventures/little_grep/main.c
+++ C/ventures/little_grep/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+// for cli, the first input is the binary name
+// and pieped inputs are to be read from stdin stream of the porgram
+
 int main(int argc, char *argv[]) {
     printf("application runs though...\n");
     printf("size: %d\n", argc);
     printf("value1: %s\n", argv[0]);
     printf("value2: %s\n", argv[1]);
+
+
+    char inp[1024];
+
+    int ret = *fgets(inp, sizeof(inp), stdin);
+
+    printf("ret int: %x\n", ret);
+    printf("str from stdid: %s\n", inp);
 }
